0c5e4bf8123952d7d2bfdf214eea6b581da3a090
João Pedro Canal <joaopedrinho2909@hotmail.com>
2025-06-26 19:27:38 -0300

    Create hangman_words.py

---
+++ Day 7/hangman_words.py
@@ -0,0 +1,8 @@
+word_list = [
+    "bulbasaur", "ivysaur", "venusaur", "charmander", "charmeleon",
+    "charizard", "squirtle", "wartortle", "blastoise", "caterpie",
+    "metapod", "butterfree", "weedle", "kakuna", "beedrill",
+    "pidgey", "pidgeotto", "pidgeot", "rattata", "raticate",
+    "spearow", "fearow", "ekans", "arbok", "pikachu",
+    "raichu", "sandshrew", "sandslash", "nidoran♀", "nidorina"
+]
